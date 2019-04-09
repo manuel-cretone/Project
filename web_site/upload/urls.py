c8abb4aa0e44c8056aad97f94d87dff7cea4794d
manuel cretone <manuel.cretone@studenti.unicam.it>
2019-04-10 00:10:36 +0200

    print some signals

--- web_site/upload/urls.py
+++ web_site/upload/urls.py
@@ -1,7 +1,15 @@
+from django.conf.urls.static import static
 from django.urls import path
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     # il nome si riferisce a questo particolare ULR mappato
     path('', views.index, name='index'),
+    path('update', views.simple_upload, name='loaded')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- web_site/upload/views.py
+++ web_site/upload/views.py
@@ -6,14 +6,38 @@ import os as os
 # Create your views here.
 
 
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'index.html', {
+            'uploaded_file_url': uploaded_file_url
+
+        })
+    return render(request, 'index.html')
+
+
 def index(request):
 
     f = pyedflib.EdfReader(
         './prova.edf')
     n = f.signals_in_file
-
+    print("SIGNALS =>")
+    print(n)
     signal_read = f.getSignalHeaders()
+    m = f.getDigitalMaximum()
+    total_signal = f.readSignal(0, 0, 1000)
+
     signal_labels = f.getSignalLabels()
+    time = f.samples_in_datarecord
+    """f.samples_in_datarecord"""
     sigbufs = np.zeros((n, f.getNSamples()[0]))
     for i in np.arange(n):
         sigbufs[i, :] = f.readSignal(i)
@@ -22,7 +46,10 @@ def index(request):
     context = {
         'file': sigbufs,
         'signal': signal_labels,
-        'read': signal_read
+        'read': signal_read,
+        'm': m,
+        'total_signal': total_signal,
+        'time': time
     }
 
-    return render(request, 'index.html', context=context)
+    return render(request, 'loaded.html', context=context)
