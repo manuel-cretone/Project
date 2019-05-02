51f1858685f696ab0570d6bde8c53b254a01af78
arth-mod-loc <emilio.silvestri@studenti.unicam.it>
2019-05-02 18:56:42 +0200

    fixed urls

--- Project/newupload/urls.py
+++ Project/newupload/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Upload.as_view(), name="upload_file"),
-    path('values', views.Values.as_view(), name="getValues"),
-    path('statistics', views.Statistics.as_view(), name="getStatistics"),
+    path('values/', views.Values.as_view(), name="getValues"),
+    path('statistics/', views.Statistics.as_view(), name="getStatistics"),
     # re_path(r'^getparam/$', views.manageParam, name="manageParam")
 ]
